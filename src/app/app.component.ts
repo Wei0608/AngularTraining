dc2cb6dea2c1ffe945413b2e5b7e4beb7eb1e491
Wei <redhairjake37@gmail.com>
2023-06-08 21:23:08 +0800

    ngStyle Exercise not complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AngularTraining';
+  red: number = 0;
+  green: number = 0;
+  blue: number = 0;
+
+  updateRed(event: Event) {
+    this.red = +(<HTMLInputElement>event.target).value;
+  }
+
+  updateGreen(event: Event) {
+    this.green = +(<HTMLInputElement>event.target).value;
+  }
+
+  updateBlue(event: Event) {
+    this.blue = +(<HTMLInputElement>event.target).value;
+  }
 }
